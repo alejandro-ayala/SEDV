e59d1a59c20372ba8dab228bafdf2fbc119d6a11
Alejandro <alejandro.ayala@alumnos.upm.es>
2022-03-09 23:12:40 +0100

    First version of node 2

--- Src/main_nodo1.c
+++ Src/main_nodo1.c
@@ -62,66 +62,64 @@ ADC_HandleTypeDef hadc1;
 CAN_HandleTypeDef hcan1;
 SPI_HandleTypeDef hspi1;
 
-TaskHandle_t TiltHeadTaskHandle = NULL;
-TaskHandle_t SteeringWheelTaskHandle = NULL;
-TaskHandle_t RelaxDetectionTaskHandle = NULL;
-TaskHandle_t RiskDetectionTaskHandle = NULL;
-TaskHandle_t UpdateModeTaskHandle = NULL;
-SemaphoreHandle_t xSemaphoreUpdateMode = NULL;
+TaskHandle_t VelocityTaskHandle = NULL;
+TaskHandle_t DistanceTaskHandle = NULL;
+TaskHandle_t CarBrakeTaskHandle = NULL;
+TaskHandle_t DippedHealLightTaskHandle = NULL;
+SemaphoreHandle_t xMutexVelocity = NULL;
+#define NOTIFICATION_TASK
+#ifndef NOTIFICATION_TASK
+SemaphoreHandle_t xSemaphoreBrake = NULL;
+#endif
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_SPI1_Init(void);
 static void MX_CAN1_Init(void);
-void InitLIS3DSHI(void);
 
 /* Periodic task */
-void TiltHeadTask(void const * argument);
-void SteeringWheelTask(void const * argument);
-void RelaxDetectionTask(void const * argument);
-void RiskDetectionTask(void const * argument);
-void UpdateModeTask(void const * argument);
+void VelocityTask(void const * argument);
+void DistanceTask(void const * argument);
+void CarBrakeTask(void const * argument);
+void DippedHeadlightsTask(void const * argument);
 
-
-float currentSteeringWheelAngle = 0;
-float previousSteeringWheelAngle = 0;
-float tempSteeringWheelAngle = 0;
-
-uint8_t steeringWhellGrip = 0;
-uint8_t spiTxBuf[2], spiRxBuf[2];
-uint8_t SPI_Read (uint8_t address);
-int Ix, Iy, Iz;
-uint8_t Ix1, Ix2;
-uint8_t Iy1, Iy2;
-uint8_t Iz1, Iz2;
-double X, Y, Z;
-double currentRotX, currentRotY;
-double previousRotX, previousRotY;
-int riskSmartphoneUse = 0, riskDistraction = 0, riskDrowsiness = 0, riskLevel2 = 0;	
-int relaxSymptom = 0;
-int swerveSymptom = 0;
-int drowsinessSymptom = 0;
-int functionMode = 0;
+/* Auxiliar functions */
+void TurnOnBrakeLight(int brakeState);
 /* Variables para depuracion */
 int ContTarea1 = 0;
 int ContTarea2 = 0;
 
+float currentVelocity = 0;
+float previousVelocity = 0;
+float averageVelocity = 0;
+int dippedHeadlight = 0;
+int currentBrakeLevel = 0;
+float distance = 100.0;
+float safeDistance=50.0;
+
+int rawCurrentVelocity = 0;
 
 /*Prioridades de las Tareas Periodicas*/
 #define PR_TAREA1 2
 
 /*Task period*/
-#define TILT_HEAD_TASK_PERIOD 600
-#define STEERING_WHEEL_TASK_PERIOD 400
-#define RELAX_DETECTION_TASK_PERIOD 500
-#define RYSK_DETECTION_TASK_PERIOD 300
+#define VELOCITY_TASK_PERIOD 250
+#define DISTANCE_TASK_PERIOD 300
+#define CAR_BRAKE_TASK_PERIOD 150
+#define DIPPED_HEADLIGHTS_TASK_PERIOD 1000
 
 #define TRUE 1
 #define FALSE 0
 
 #define STACK_SIZE 128
 
+
+#define BRAKE_LEVEL_1 0
+#define BRAKE_LEVEL_2 1
+#define BRAKE_LEVEL_3 2
+#define BRAKE_LEVEL_4 3
+#define BRAKE_OFF     0xFF
 /**
   * @brief  The application entry point.
   * @retval int
@@ -142,17 +140,29 @@ int main(void)
   MX_ADC1_Init();
   MX_SPI1_Init();
   MX_CAN1_Init();
-	InitLIS3DSHI();	 
+	
+	
+  xMutexVelocity = xSemaphoreCreateMutex( );
+	
+ if( xMutexVelocity == NULL )
+ {
+	 //TODO: Handle error in creating semaphore
+ }
+#ifndef NOTIFICATION_TASK 
+	xSemaphoreBrake = xSemaphoreCreateBinary();
+	 if( xSemaphoreBrake == NULL )
+	 {
+		 //TODO: Handle error in creating semaphore
+	 } 
+#endif	 
   /* Create the thread(s) */
   /* definition and creation of Tarea1 */
-  xSemaphoreUpdateMode = xSemaphoreCreateBinary();
-	xTaskCreate( TiltHeadTask, "TiltHeadTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &TiltHeadTaskHandle );   
-	xTaskCreate( SteeringWheelTask, "SteeringWheel", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &SteeringWheelTaskHandle );
-	xTaskCreate( RelaxDetectionTask, "RelaxDetectionTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &RelaxDetectionTaskHandle );   
-	xTaskCreate( RiskDetectionTask, "RiskDetectionTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &RiskDetectionTaskHandle );   
-	xTaskCreate( UpdateModeTask, "UpdateModeTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &UpdateModeTaskHandle );   
 
-
+	xTaskCreate( VelocityTask, "VelocityTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &VelocityTaskHandle );   
+	xTaskCreate( DistanceTask, "DistanceTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &DistanceTaskHandle );
+	xTaskCreate( CarBrakeTask, "CarBrakeTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &CarBrakeTaskHandle );   
+	xTaskCreate( DippedHeadlightsTask, "DippedHeadlightsTask", STACK_SIZE, ( void * ) 1, tskIDLE_PRIORITY, &DippedHealLightTaskHandle );
+ 
   /* Start scheduler */
   osKernelStart();
   
@@ -420,7 +430,7 @@ static void MX_GPIO_Init(void)
   * @retval None
   */
 /* USER CODE END Header_StartTarea1 */
-void TiltHeadTask(void const * argument)
+void VelocityTask(void const * argument)
 {
 
 	TickType_t xLastWakeTime;
@@ -429,51 +439,6 @@ void TiltHeadTask(void const * argument)
   for(;;)
   {
 		xLastWakeTime = xTaskGetTickCount();
-
-		Ix1 = SPI_Read (0x28);
-		Ix2 = SPI_Read (0x29);
-		Ix = (Ix2 << 8) + Ix1;
-		if (Ix >= 0x8000) Ix = -(65536 - Ix);
-		X = Ix/16384.0;
-			
-		Iy1 = SPI_Read (0x2A);
-		Iy2 = SPI_Read (0x2B);
-		Iy = (Iy2 << 8) + Iy1;
-		if (Iy >= 0x8000) Iy = -(65536 - Iy);
-		Y = Iy/16384.0;
-			
-		Iz1 = SPI_Read (0x2C);
-		Iz2 = SPI_Read (0x2D);
-		Iz = (Iz2 << 8) + Iz1;
-		if (Iz >= 0x8000) Iz = -(65536 - Iz);
-		Z = Iz/16384.0;
-
-		previousRotX = currentRotX;
-		previousRotY = currentRotY;	
-		currentRotX = atan2(Y, sqrt(X*X+Z*Z)) * 180.0/3.1416;
-		currentRotY = - atan2(X, sqrt(Y*Y+Z*Z)) * 180.0/3.1416;	
-		//TODO: add check in the X axis with the angle of steering wheel
-    if(((abs((int)previousRotY) > 30) && (abs((int)currentRotY) > 30))		|| ((abs((int)previousRotX) > 30) && (abs((int)currentRotX) > 30))	)
-		{
-			drowsinessSymptom = 1;
-		}
-		else
-		{
-			drowsinessSymptom = 0;		
-		}
-		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( TILT_HEAD_TASK_PERIOD )); 
-  }
-}
-
-void SteeringWheelTask(void const * argument)
-{
-	TickType_t xLastWakeTime;
-	int cntSwerveSymptom = 0;
-  /* Infinite loop */
-  for(;;)
-  {
-		xLastWakeTime = xTaskGetTickCount();
-
 	/* Lectura del canal ADC0 */
 		ADC_ChannelConfTypeDef sConfig = {0};
 		sConfig.Channel = ADC_CHANNEL_0; // seleccionamos el canal 0
@@ -488,109 +453,133 @@ void SteeringWheelTask(void const * argument)
 		HAL_ADC_Start(&hadc1); 
 		if(HAL_ADC_PollForConversion(&hadc1, 5) == HAL_OK)
 		{
-				previousSteeringWheelAngle = currentSteeringWheelAngle;
-				tempSteeringWheelAngle = HAL_ADC_GetValue(&hadc1);
-			  currentSteeringWheelAngle = (((tempSteeringWheelAngle * 360)/1023) - 180);
+				previousVelocity = currentVelocity;
+				if( xSemaphoreTake( xMutexVelocity, ( TickType_t ) 1 ) == pdTRUE )
+				{			
+				    rawCurrentVelocity = HAL_ADC_GetValue(&hadc1);
+					  currentVelocity = (rawCurrentVelocity * 200.0)/1023.0;
+    				averageVelocity = (previousVelocity + currentVelocity) / 2.0;			
+					  xSemaphoreGive( xMutexVelocity );
+				}
 		}
-		if(abs(previousSteeringWheelAngle - currentSteeringWheelAngle))
-		{
-			cntSwerveSymptom++;
-			if(cntSwerveSymptom >= 2)
-			{
-				swerveSymptom = 1;			
-			}
-		}
-		else
-		{
-			//TODO: start timer and set to 0 after 5s
-		  cntSwerveSymptom = 0;
-			swerveSymptom = 0;
-		}
-		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( STEERING_WHEEL_TASK_PERIOD ));		
+				
+		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( VELOCITY_TASK_PERIOD )); 
   }
 }
 
-void RelaxDetectionTask(void const * argument)
-{
-  TickType_t xLastWakeTime;
-	int cntSteeringWhellGrip = 0;
-  for(;;)
-  {
-		xLastWakeTime = xTaskGetTickCount();
-		steeringWhellGrip = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_1);
-		if(steeringWhellGrip == 1)
-		{
-			cntSteeringWhellGrip++;
-			if(cntSteeringWhellGrip >= 3)
-			{
-				relaxSymptom = 1;
-			}
-		}
-		else
-		{
-			//TODO: add remove the symphom after 2 read consecutive
-			relaxSymptom = 0;
-		}
-		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( RELAX_DETECTION_TASK_PERIOD ));
-	}		
-}
-
-void RiskDetectionTask(void const * argument)
+void DistanceTask(void const * argument)
 {
 	TickType_t xLastWakeTime;
+	
+  /* Infinite loop */
   for(;;)
   {
 		xLastWakeTime = xTaskGetTickCount();
-	
-	
-		if((abs((int)currentRotY) > 20) && (abs((int)currentRotX) > 20) && (relaxSymptom == 1))
-		{
-		   riskSmartphoneUse = 1;
-			 //TODO: do the actions
+		if( xSemaphoreTake( xMutexVelocity, ( TickType_t ) 1 ) == pdTRUE )
+		{			
+			ContTarea2 ++;
+      //Calculate the safe distance according to the current velocity 
+			safeDistance = pow((averageVelocity/10.0),2); 
+			
+			//If the current distance is lower than the safe distance, the Brake task is triggered
+			if(distance < safeDistance)
+			{
+				//TODO: asign the correct brake level value
+				  if((distance/safeDistance) >= 0.75)currentBrakeLevel = BRAKE_LEVEL_1;
+				  else if((distance/safeDistance) >= 0.5)currentBrakeLevel = BRAKE_LEVEL_2;
+				  else if((distance/safeDistance) >= 0.25)currentBrakeLevel = BRAKE_LEVEL_3;
+				  else if((distance/safeDistance) >= 0)currentBrakeLevel = BRAKE_LEVEL_4;
+#ifndef NOTIFICATION_TASK          
+			    xSemaphoreGive( xSemaphoreBrake );
+#else
+					xTaskNotifyGive(CarBrakeTaskHandle);
+					taskYIELD();
+#endif
+			}
+			else //If the distance is high, take the semaphore to block the brake task
+			{
+					TurnOnBrakeLight(BRAKE_OFF);
+				//TODO: Check if we should block the timeout until the brake task release it 
+#ifndef NOTIFICATION_TASK				
+			    xSemaphoreTake( xSemaphoreBrake, ( TickType_t ) 1 );
+#endif				
+			}
+			
+			xSemaphoreGive( xMutexVelocity );
 		}
-		else
-		{
-		   riskSmartphoneUse = 0;		
-		}
-		if(((abs((int)currentRotY) > 20) || (abs((int)currentRotX) > 20)) && (relaxSymptom == 0))
-		{
-		   riskDistraction = 1;	
-		  //TODO: do the actions			
-		}
-		else
-		{
-		   riskDistraction = 0;					
-		}
-	
-		if(drowsinessSymptom == 1)
-		{
-			riskDrowsiness = 1;
-		  //TODO: do the actions
-		}
-		else
-		{
-			riskDrowsiness = 0;		
-		}
+		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( DISTANCE_TASK_PERIOD ));
 
-		if((riskDrowsiness + riskDistraction + riskSmartphoneUse) >= 2)
-		{
-			riskLevel2 = 1;
-		}
-		else
-		{
-			riskLevel2 = 0;		
-		}
-		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( RYSK_DETECTION_TASK_PERIOD ));
+		
+  }
+}
+
+
+void CarBrakeTask(void const * argument)
+{
+  TickType_t xLastWakeTime;
+	
+  for(;;)
+  {
+		xLastWakeTime = xTaskGetTickCount();
+		// Block waiting for the semaphore to become available.
+#ifndef NOTIFICATION_TASK		
+		if( xSemaphoreTake( xSemaphoreBrake, 0xFFFF ) == pdTRUE )
+#else
+		ulTaskNotifyTake( pdTRUE, portMAX_DELAY);		
+#endif		
+		{	
+			//TODO: activate the car brake with the current level
+			TurnOnBrakeLight(currentBrakeLevel);
+			
+#ifndef NOTIFICATION_TASK
+			xSemaphoreGive( xSemaphoreBrake );
+#endif			
+		}			
+		
+		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( CAR_BRAKE_TASK_PERIOD ));
 	}		
 }
 
+void DippedHeadlightsTask(void const * argument)
+{
+  TickType_t xLastWakeTime;
+	
+  for(;;)
+  {
+		xLastWakeTime = xTaskGetTickCount();
+		
+		ADC_ChannelConfTypeDef sConfig = {0};
+		sConfig.Channel = ADC_CHANNEL_2; 
+		sConfig.Rank = 1;
+		sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
+		HAL_ADC_ConfigChannel(&hadc1, &sConfig);
+		if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+		{
+			Error_Handler();
+		}		
+		HAL_ADC_Start(&hadc1); 
+		if(HAL_ADC_PollForConversion(&hadc1, 5) == HAL_OK)
+		{
+				dippedHeadlight = HAL_ADC_GetValue(&hadc1);
+			  if(dippedHeadlight < 100)
+				{
+				    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
+				}
+				else
+				{
+				    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);				
+				}
+					
+		}		
+		
+		vTaskDelayUntil( &xLastWakeTime, pdMS_TO_TICKS( DIPPED_HEADLIGHTS_TASK_PERIOD ));
+	}	
+}
 /* Funcion para el tratamiento de interrupciones */
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   long yield = pdFALSE;
-	portBASE_TYPE taskWoken = pdFALSE;
-	xSemaphoreGiveFromISR( xSemaphoreUpdateMode,&taskWoken );
 //  /* Prevent unused argument(s) compilation warning */
   UNUSED(GPIO_Pin);
   portYIELD_FROM_ISR(yield);
@@ -617,60 +606,43 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE END Callback 1 */
 }
 
-void InitLIS3DSHI(void)
+void TurnOnBrakeLight(int brakeState)
 {
-	 /*To transmit data in SPI follow the next steps: */
-	 // 1. Bring slave select to low
-	 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
-	 // 2. Transmit register + data
-	 spiTxBuf[0] = 0x20; // control Register
-	 spiTxBuf[1] = 0x17; //Data  Enable X Y Z Rate 3.125 Hz --- Valor original = 0x11
-	 //								size, timeout
-	 HAL_SPI_Transmit(&hspi1, spiTxBuf, 2, 50);
-	 // 3. Bring slave select high
-	 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
-
-	 /*To receive data in SPI follow the next steps: */
-	 // 1.Bring slave select low
-	 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
-	 // 2.Transmit register + 0x80 (To set MSB high) Most Significant Bit(MSB) high = read mode
-	 spiTxBuf[0] = 0x20|0x80; //Register
-	 HAL_SPI_Transmit(&hspi1, spiTxBuf, 1, 50);
-	 // 3.Receive data
-	 HAL_SPI_Receive(&hspi1, spiRxBuf, 1, 50);
-	 // 4.Bring slave select high
-	 HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
-
-}
-
-uint8_t SPI_Read (uint8_t address)
-{
-	  // 1.Bring slave select low
-	    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);
-	    // 2.Transmit register + 0x80 (To set MSB high) Most Significant Bit(MSB) high = read mode
-	    spiTxBuf[0] = address|0x80; //Register
-	    HAL_SPI_Transmit(&hspi1, spiTxBuf, 1, 50);
-	    // 3.Receive data
-	    HAL_SPI_Receive(&hspi1, spiRxBuf, 1, 50);
-	    // 4.Bring slave select high
-	    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
-
-        return spiRxBuf[0];
-}
-
-void UpdateModeTask(void const * argument)
-{
-  for(;;)
-  {
-		if( xSemaphoreTake( xSemaphoreUpdateMode, 0xFFFF ) == pdTRUE )
-		{
-			functionMode++;
-			if(functionMode>=3)
+			switch (brakeState)
 			{
-				functionMode = 0;
+				case BRAKE_LEVEL_1:
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);					
+					break;
+				case BRAKE_LEVEL_2:
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);	
+				break;
+				case BRAKE_LEVEL_3:
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);	
+				break;
+				case BRAKE_LEVEL_4:
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);	
+				break;
+				
+				default:
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);					
+						HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);						
+					break;
+					
 			}
-		}
-	}
 }
 /**
   * @brief  This function is executed in case of error occurrence.
